0f91d588ccd541398ad508977b91202a843616ed
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-09 22:08:55 +0300

    add dni nedeli

---
+++ 001_DenNedeli/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int a = int.Parse(Console.ReadLine());
+if (a > 5 && a < 8)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (a < 6)
+{
+    Console.WriteLine("Будний день");
+}
+if (a > 7) 
+{
+    Console.WriteLine("Ошибка");
+}
