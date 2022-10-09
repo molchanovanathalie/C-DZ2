35e35fe695c85c2bd59683545699d76ab5e5f304
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-09 22:13:19 +0300

    add kvadrat chisla

---
+++ 002_Kvadratchisla/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число а: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a == b*b)
+{
+    Console.WriteLine("Число " + a + " является квадратом числа " + b);
+}
+else
+{
+    Console.WriteLine("Число " + a + " не является квадратом числа " + b);
+}
+if(b == a*a)
+{
+    Console.WriteLine("Число " + b + " является квадратом числа " + a);
+}
+else
+{
+    Console.WriteLine("Число " + b + " не является квадратом числа " + a);
+}
