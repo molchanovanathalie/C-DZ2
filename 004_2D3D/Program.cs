13adfc1322dc8fe974cc5f8fe8e91909c5a2d725
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-09 22:32:04 +0300

    add 2d 3d

---
+++ 004_2D3D/Program.cs
@@ -0,0 +1,11 @@
+﻿double Distance2D(int xa, int ya, int xb, int yb)
+{
+    return Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2));
+}
+Console.WriteLine(Distance2D(6, 6, 4, 4));
+
+double Distance3D(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    return Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2) + Math.Pow((za - zb), 2));
+}
+Console.WriteLine(Distance3D(6, 6, 4, 4, 7, 7));
