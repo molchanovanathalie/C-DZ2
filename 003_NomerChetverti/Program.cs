1f67da064d2e1488c43675af6ab93b4a26bbdd6c
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-09 22:17:11 +0300

    add nomer chetverti

---
+++ 003_NomerChetverti/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N == 1)
+{
+    Console.WriteLine("x = (0; infinity); y = (0; infinity)");
+}
+if (N == 2)
+{
+    Console.WriteLine("x = (-infinity; 0); y = (0; infinity)");
+}
+if (N == 3)
+{
+    Console.WriteLine("x = (-infinity; 0); y = (-infinity; 0)");
+}
+if (N == 4)
+{
+    Console.WriteLine("X = (0; infinity); Y = (-infinity; 0)");
+}
+if (N < 1 | N > 4)
+{
+    Console.WriteLine("Число N должно быть от 1 до 4");
+}
